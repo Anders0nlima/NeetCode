754e64ad112e128b4357a9866ab62120b8f26391
Anders0nlima <andersoncon85@gmail.com>
2025-01-06 19:49:09 -0300

    twosum

--- src/ArraysHashing/TwoSum.java
+++ src/ArraysHashing/TwoSum.java
@@ -1,7 +1,31 @@
 package ArraysHashing;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
+
+    public int[] TwoSum(int[] nums, int target){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[0];
+
+    }
+
+
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+        int[] nums = {1, 2, 3, 4};
+        int target = 5;
+        int[] result = twoSum.TwoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
 
 }
